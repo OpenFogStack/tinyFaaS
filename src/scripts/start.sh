57d48d8725b161393b67503e645eb9bee1e06fa2
Emily Dietrich <jakibaki@live.com>
2020-06-02 14:27:15 +0200

    added helper scripts

---
+++ src/scripts/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+./scripts/cleanup.sh
+docker build -t tinyfaas-mgmt .
+docker run -v /var/run/docker.sock:/var/run/docker.sock -p 8080:8080 --name tinyfaas-mgmt -d tinyfaas-mgmt tinyfaas-mgmt
+
+sleep 1
+
+curl http://localhost:8080 --data '{"path": "sieve-of-erasthostenes", "resource": "/sieve/primes", "entry": "sieve.js", "threads": 4}'
+
+sleep 1
+aiocoap-client coap://localhost/sieve/primes
