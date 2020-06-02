db373b682960171331e60034a6ed9c62b91fbf70
Emily Dietrich <jakibaki@live.com>
2020-06-02 14:48:32 +0200

    Added script for obtaining logs

--- src/handlers/sieve-of-erasthostenes/sieve.js
+++ src/handlers/sieve-of-erasthostenes/sieve.js
@@ -3,7 +3,9 @@ module.exports = {
     const max = 1000;
     let sieve = [], i, j, primes = [];
     for (i = 2; i <= max; ++i) {
+
         if (!sieve[i]) {
+            console.log("Found prime: " + i);
             primes.push(i);
             for (j = i << 1; j <= max; j += i) {
                 sieve[j] = true;
---
+++ src/scripts/get_logs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for line in $(docker network ls --filter name=handler-net -q) ; do
+    for cont in $(docker ps -a -q  --filter network=$line) ; do
+        docker logs $cont
+    done
+done
