be02c735feaeec8cbe4f5f3cdeb9f165c6c64882
Emily Dietrich <jakibaki@live.com>
2020-06-02 19:43:23 +0200

    improved scripts

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -3,9 +3,7 @@
 docker build -t tinyfaas-mgmt .
 docker run -v /var/run/docker.sock:/var/run/docker.sock -p 8080:8080 --name tinyfaas-mgmt -d tinyfaas-mgmt tinyfaas-mgmt
 
-sleep 1
+sleep 5
 
 curl http://localhost:8080 --data '{"path": "sieve-of-erasthostenes", "resource": "/sieve/primes", "entry": "sieve.js", "threads": 4}'
-
-sleep 1
-aiocoap-client coap://localhost/sieve/primes
+#aiocoap-client coap://localhost/sieve/primes
