3dec3489030e33c1e4d7a6038da50343e3a0566f
Tobias Pfandzelter <tp@mcc.tu-berlin.de>
2023-07-28 10:53:36 +0200

    improve cleaning script

---
+++ clean.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -e
+
+TF_TAG="tinyFaaS"
+TMP_DIR="tmp"
+
+# remove old containers, networks and images
+containers=$(docker ps -a -q --filter label=$TF_TAG)
+
+if [ -n "$containers" ]; then
+    docker stop "$containers" > /dev/null
+    docker rm "$containers" > /dev/null
+else
+    echo "No old containers to remove. Skipping..."
+fi
+
+networks=$(docker network ls -q --filter label=$TF_TAG)
+
+if [ -n "$networks" ]; then
+    docker network rm "$networks" > /dev/null
+else
+    echo "No old networks to remove. Skipping..."
+fi
+
+images=$(docker image ls -q --filter label=$TF_TAG)
+
+if [ -n "$images" ]; then
+    docker rmi "$images" > /dev/null
+else
+    echo "No old images to remove. Skipping..."
+fi
+
+# remove tmp directory
+if [ -d "$TMP_DIR" ]; then
+    rm -rf "$TMP_DIR"
+else
+    echo "No tmp directory to remove. Skipping..."
+fi
+
+set +e
