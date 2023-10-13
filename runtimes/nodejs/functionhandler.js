feb3cb57f5076c1cb0e2783975eb6879472a21d7
valentin-carl <vacleantrinl@googlemail.com>
2023-10-13 11:15:53 +0200

    modify nodejs runtime to make request bodies accessible

--- runtimes/nodejs/functionhandler.js
+++ runtimes/nodejs/functionhandler.js
@@ -3,7 +3,14 @@ process.chdir("fn");
 
 const handler = require("fn");
 const express = require("express");
+const bodyParser = require("body-parser")
 const app = express();
+
+app.use(bodyParser.text({
+    type: function(req) {
+        return 'text';
+    }
+}));
 
 app.all("/health", (req, res) => {
   return res.send("OK");
